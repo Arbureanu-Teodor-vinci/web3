f28764b1e0c1ff0c110fa238fc4e9256f87084ad
Arbureanu-Teodor-vinci <teodor.arbureanu@student.vinci.be>
2024-09-17 16:31:13 +0200

    1.1-1.2 : refactor - modules

---
+++ exercices/module1/ex1.1/src/components/Content/Content.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line react-refresh/only-export-components, no-unused-vars
+const Content = (props) => {
+    return (
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    )
+  }
---
+++ exercices/module1/ex1.1/src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars, react-refresh/only-export-components
+const Header = (props) => {
+    return (
+      <h1>{props.course}</h1>
+    )
+  }
