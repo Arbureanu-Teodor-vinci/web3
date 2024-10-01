bc704c0600eb498ca9e9f55b6c1859d2c8f63fe0
Arbureanu-Teodor-vinci <teodor.arbureanu@student.vinci.be>
2024-10-01 16:08:42 +0200

    new:ex2.1

--- exercices/module2/2.1/src/components/App/App.jsx
+++ exercices/module2/2.1/src/components/App/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ClickCounter from "components/ClickCounter/ClickCounter";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
       <div>
@@ -17,19 +15,16 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <ClickCounter
+        title="Amazing counter"
+        on10ClickMessage="You are a master in the art of clicking !"
+        onMouseOverMessage="Please click on me now !"
+      />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
