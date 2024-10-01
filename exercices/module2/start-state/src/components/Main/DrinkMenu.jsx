469b24f83f75880024a127cac7c88f7aeed5e05a
Arbureanu-Teodor-vinci <teodor.arbureanu@student.vinci.be>
2024-10-01 15:31:45 +0200

    tutorial module2

---
+++ exercices/module2/start-state/src/components/Main/DrinkMenu.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+const DrinkMenu = ({ title, children }) => {
+  return (
+    <div className="drink-menu">
+      <h4>{title}</h4>
+      <div className="drink-items">{children}</div>
+    </div>
+  );
+};
+
+export default DrinkMenu;
