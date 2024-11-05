384d1520d7d947e31a6e599eaad776ed9b7507e1
Arbureanu-Teodor-vinci <teodor.arbureanu@student.vinci.be>
2024-11-05 16:08:23 +0100

    module5 all

---
+++ exercices/module5/tuto/src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import AppLoader from 'components/AppLoader/AppLoader';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AppLoader />);
